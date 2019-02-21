e4ed24a893f632b7e77771e920109a4906f7db64
Mark McLoughlin <markmc@redhat.com>
2019-02-21 12:46:37 +0000

    baremetal: stub out machines integration
    FIXME: use baremetal provider spec

--- pkg/types/baremetal/validation/platform.go
+++ pkg/types/baremetal/validation/platform.go
@@ -9,5 +9,8 @@ import (
 // ValidatePlatform checks that the specified platform is valid.
 func ValidatePlatform(p *baremetal.Platform, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
+	if p.DefaultMachinePlatform != nil {
+		allErrs = append(allErrs, ValidateMachinePool(p.DefaultMachinePlatform, fldPath.Child("defaultMachinePlatform"))...)
+	}
 	return allErrs
 }
