b585c507c96f677c858bdd45554e0aea897a2499
Mark McLoughlin <markmc@redhat.com>
2019-02-21 12:46:37 +0000

    baremetal: stub out machines integration
    FIXME: use baremetal provider spec
    
    Co-authored-by: Stephen Benjamin <stephen@redhat.com>

--- pkg/types/baremetal/platform.go
+++ pkg/types/baremetal/platform.go
@@ -3,4 +3,9 @@ package baremetal
 // Platform stores all the global configuration that all
 // machinesets use.
 type Platform struct {
+	// DefaultMachinePlatform is the default configuration used when
+	// installing on bare metal for machine pools which do not define their own
+	// platform configuration.
+	// +optional
+	DefaultMachinePlatform *MachinePool `json:"defaultMachinePlatform,omitempty"`
 }
