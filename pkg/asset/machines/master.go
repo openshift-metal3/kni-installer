8ed93cbfbbf1bcc01e400bbe1a2b9f39eafefbec
Mark McLoughlin <markmc@redhat.com>
2019-02-21 12:46:37 +0000

    baremetal: stub out machines integration
    FIXME: use baremetal provider spec

--- pkg/asset/machines/master.go
+++ pkg/asset/machines/master.go
@@ -25,6 +25,7 @@ import (
 	"github.com/openshift-metalkube/kni-installer/pkg/asset/installconfig"
 	"github.com/openshift-metalkube/kni-installer/pkg/asset/machines/aws"
 	"github.com/openshift-metalkube/kni-installer/pkg/asset/machines/azure"
+	"github.com/openshift-metalkube/kni-installer/pkg/asset/machines/baremetal"
 	"github.com/openshift-metalkube/kni-installer/pkg/asset/machines/libvirt"
 	"github.com/openshift-metalkube/kni-installer/pkg/asset/machines/machineconfig"
 	"github.com/openshift-metalkube/kni-installer/pkg/asset/machines/openstack"
@@ -156,7 +157,14 @@ func (m *Master) Generate(dependencies asset.Parents) error {
 		azure.ConfigMasters(machines, clusterID.InfraID)
 	case baremetaltypes.Name:
 		// FIXME: baremetal
-		return nil
+		mpool := defaultBareMetalMachinePoolPlatform()
+		mpool.Set(ic.Platform.BareMetal.DefaultMachinePlatform)
+		mpool.Set(pool.Platform.BareMetal)
+		pool.Platform.BareMetal = &mpool
+		machines, err = baremetal.Machines(clusterID.InfraID, ic, pool, "master", "master-user-data")
+		if err != nil {
+			return errors.Wrap(err, "failed to create master machine objects")
+		}
 	case nonetypes.Name, vspheretypes.Name:
 	default:
 		return fmt.Errorf("invalid Platform")
