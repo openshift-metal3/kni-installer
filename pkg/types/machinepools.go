c7a6e01b6c949ac71acf4b6ab64a053eae74fc25
Mark McLoughlin <markmc@redhat.com>
2019-02-21 12:46:34 +0000

    baremetal: add stub baremetal platform

--- pkg/types/machinepools.go
+++ pkg/types/machinepools.go
@@ -3,6 +3,7 @@ package types
 import (
 	"github.com/openshift-metalkube/kni-installer/pkg/types/aws"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/azure"
+	"github.com/openshift-metalkube/kni-installer/pkg/types/baremetal"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/libvirt"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/openstack"
 	"github.com/openshift-metalkube/kni-installer/pkg/types/vsphere"
@@ -55,6 +56,9 @@ type MachinePoolPlatform struct {
 
 	// Azure is the configuration used when installing on OpenStack.
 	Azure *azure.MachinePool `json:"azure,omitempty"`
+
+	// BareMetal is the configuration used when installing on bare metal.
+	BareMetal *baremetal.MachinePool `json:"openstack,omitempty"`
 }
 
 // Name returns a string representation of the platform (e.g. "aws" if
@@ -74,6 +78,8 @@ func (p *MachinePoolPlatform) Name() string {
 		return vsphere.Name
 	case p.Azure != nil:
 		return azure.Name
+	case p.BareMetal != nil:
+		return baremetal.Name
 	default:
 		return ""
 	}
