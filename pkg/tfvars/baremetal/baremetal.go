d9180929b549511e2f93237458d0afb4e5912199
Mark McLoughlin <markmc@redhat.com>
2019-02-22 07:26:55 +0000

    baremetal: launch the bootstrap VM with libvirt
    Use libvirt to launch the bootstrap VM. Based on the bootstrap config
    from libvirt platform driver.
    
    The libvirt platform driver creates a base volume, and then creates
    copy-on-write volumes for various VMs it creates. The baremetal
    platform driver only creates a single libvirt VM, so it does not need
    this additional layer.

--- pkg/tfvars/baremetal/baremetal.go
+++ pkg/tfvars/baremetal/baremetal.go
@@ -3,13 +3,31 @@ package baremetal
 
 import (
 	"encoding/json"
+
+	libvirttfvars "github.com/metalkube/kni-installer/pkg/tfvars/libvirt"
+	"github.com/pkg/errors"
 )
 
 type config struct {
+	URI             string `json:"libvirt_uri,omitempty"`
+	Image           string `json:"os_image,omitempty"`
+	BareMetalBridge string `json:"baremetal_bridge,omitempty"`
+	OverCloudBridge string `json:"overcloud_bridge,omitempty"`
 }
 
 // TFVars generates bare metal specific Terraform variables.
-func TFVars() ([]byte, error) {
-	cfg := &config{}
+func TFVars(libvirtURI, osImage, baremetalBridge, overcloudBridge string) ([]byte, error) {
+	osImage, err := libvirttfvars.CachedImage(osImage)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to use cached libvirt image")
+	}
+
+	cfg := &config{
+		URI:             libvirtURI,
+		Image:           osImage,
+		BareMetalBridge: baremetalBridge,
+		OverCloudBridge: overcloudBridge,
+	}
+
 	return json.MarshalIndent(cfg, "", "  ")
 }
