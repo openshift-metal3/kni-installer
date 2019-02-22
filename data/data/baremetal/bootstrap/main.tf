b7cf7779c6571b5d7b992139dbbf7842d89a68fe
Mark McLoughlin <markmc@redhat.com>
2019-02-22 07:26:55 +0000

    baremetal: launch the bootstrap VM with libvirt
    Use libvirt to launch the bootstrap VM. Based on the bootstrap config
    from libvirt platform driver.

---
+++ data/data/baremetal/bootstrap/main.tf
@@ -0,0 +1,40 @@
+resource "libvirt_volume" "bootstrap" {
+  name           = "${var.cluster_name}-bootstrap"
+  base_volume_id = "${var.base_volume_id}"
+}
+
+resource "libvirt_ignition" "bootstrap" {
+  name    = "${var.cluster_name}-bootstrap.ign"
+  content = "${var.ignition}"
+}
+
+resource "libvirt_domain" "bootstrap" {
+  name = "${var.cluster_name}-bootstrap"
+
+  memory = "4096"
+
+  vcpu = "4"
+
+  coreos_ignition = "${libvirt_ignition.bootstrap.id}"
+
+  disk {
+    volume_id = "${libvirt_volume.bootstrap.id}"
+  }
+
+  console {
+    type        = "pty"
+    target_port = 0
+  }
+
+  cpu {
+    mode = "host-passthrough"
+  }
+
+  network_interface {
+    bridge = "${var.baremetal_bridge}"
+  }
+
+  network_interface {
+    bridge = "${var.overcloud_bridge}"
+  }
+}
