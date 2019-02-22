b78e763adb4e41cba3f547b6ea06f9cd8284d8a3
Mark McLoughlin <markmc@redhat.com>
2019-02-22 07:26:55 +0000

    baremetal: launch the bootstrap VM with libvirt
    Use libvirt to launch the bootstrap VM. Based on the bootstrap config
    from libvirt platform driver.
    
    The libvirt platform driver creates a base volume, and then creates
    copy-on-write volumes for various VMs it creates. The baremetal
    platform driver only creates a single libvirt VM, so it does not need
    this additional layer.

--- data/data/baremetal/main.tf
+++ data/data/baremetal/main.tf
@@ -1,3 +1,13 @@
-locals {
-  cluster_domain = "${var.cluster_name}.${var.base_domain}"
+provider "libvirt" {
+  uri = "${var.libvirt_uri}"
+}
+
+module "bootstrap" {
+  source = "./bootstrap"
+
+  cluster_id       = "${var.cluster_id}"
+  image            = "${var.os_image}"
+  ignition         = "${var.ignition_bootstrap}"
+  baremetal_bridge = "${var.baremetal_bridge}"
+  overcloud_bridge = "${var.overcloud_bridge}"
 }
