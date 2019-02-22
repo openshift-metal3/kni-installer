17e71a0fa5e723e942469ab5ad280993697d9006
Mark McLoughlin <markmc@redhat.com>
2019-02-22 07:26:55 +0000

    baremetal: launch the bootstrap VM with libvirt
    Use libvirt to launch the bootstrap VM. Based on the bootstrap config
    from libvirt platform driver.
    
    The libvirt platform driver creates a base volume, and then creates
    copy-on-write volumes for various VMs it creates. The baremetal
    platform driver only creates a single libvirt VM, so it does not need
    this additional layer.

--- pkg/types/baremetal/defaults/platform.go
+++ pkg/types/baremetal/defaults/platform.go
@@ -4,6 +4,15 @@ import (
 	"github.com/openshift-metalkube/kni-installer/pkg/types/baremetal"
 )
 
+// Defaults for the baremetal platform.
+const (
+	// DefaultURI is the default URI of the libvirtd connection.
+	DefaultURI = "qemu:///system"
+)
+
 // SetPlatformDefaults sets the defaults for the platform.
 func SetPlatformDefaults(p *baremetal.Platform) {
+	if p.URI == "" {
+		p.URI = DefaultURI
+	}
 }
--- pkg/types/baremetal/platform.go
+++ pkg/types/baremetal/platform.go
@@ -3,6 +3,12 @@ package baremetal
 // Platform stores all the global configuration that all
 // machinesets use.
 type Platform struct {
+	// URI is the identifier for the libvirtd connection.  It must be
+	// reachable from the host where the installer is run.
+	// +optional
+	// Default is qemu:///system
+	URI string `json:"URI,omitempty"`
+
 	// DefaultMachinePlatform is the default configuration used when
 	// installing on bare metal for machine pools which do not define their own
 	// platform configuration.
