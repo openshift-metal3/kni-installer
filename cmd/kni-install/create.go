e0a9d5feafbb87a01db473feaec5d3861e2795bb
Steven Hardy <shardy@redhat.com>
2019-02-27 15:52:32 +0000

    cmd/kni-install - exit after bootstrap launch
    Temporary hack to exit after we launch the bootstrap VM, so
    we can continue to use the dev-scripts logic to launch the
    masters etc as an interim step.

--- cmd/kni-install/create.go
+++ cmd/kni-install/create.go
@@ -104,7 +104,8 @@ var (
 					logrus.Fatal(errors.Wrap(err, "loading kubeconfig"))
 				}
 
-				logrus.Warn("FIXME!")
+				logrus.Warn("FIXME! Exiting after bootstrap cluster create for baremetal testing")
+				return
 
 				err = destroyBootstrap(ctx, config, rootOpts.dir)
 				if err != nil {
