42652068d3d9e5043ef77d446725b2ed87c32520
Stephen Benjamin <stephen@redhat.com>
2019-03-21 10:16:43 -0400

    Deploy masters with kni-install

--- pkg/types/baremetal/validation/platform.go
+++ pkg/types/baremetal/validation/platform.go
@@ -10,9 +10,18 @@ import (
 // ValidatePlatform checks that the specified platform is valid.
 func ValidatePlatform(p *baremetal.Platform, fldPath *field.Path) field.ErrorList {
 	allErrs := field.ErrorList{}
-	if err := validate.URI(p.URI); err != nil {
-		allErrs = append(allErrs, field.Invalid(fldPath.Child("uri"), p.URI, err.Error()))
+	if err := validate.URI(p.LibvirtURI); err != nil {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("libvirt_uri"), p.LibvirtURI, err.Error()))
 	}
+
+	if err := validate.URI(p.IronicURI); err != nil {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("ironic_uri"), p.LibvirtURI, err.Error()))
+	}
+
+	if p.Nodes == nil {
+		allErrs = append(allErrs, field.Invalid(fldPath.Child("nodes"), p.Nodes, "nodes is missing"))
+	}
+
 	if p.DefaultMachinePlatform != nil {
 		allErrs = append(allErrs, ValidateMachinePool(p.DefaultMachinePlatform, fldPath.Child("defaultMachinePlatform"))...)
 	}
