2d4b54cbb6a99df8dac62fd40d307834a6e13909
Yolanda Robla <yroblamo@redhat.com>
2019-04-16 12:40:24 +0200

    Rewrite terraform to support dynamic number of nodes
    Change the data structure to accept data of nodes using maps.
    This will allow to add a variable number of masters (1,3) in the future.
    Added a new dependency on rodaine/hclencoder, to render terraform data
    in HCL instead of JSON, to avoid a bug in JSON parsing:
    https://github.com/hashicorp/terraform/issues/15549

--- pkg/tfvars/baremetal/baremetal.go
+++ pkg/tfvars/baremetal/baremetal.go
@@ -2,42 +2,30 @@
 package baremetal
 
 import (
-	"encoding/json"
-	"github.com/openshift-metalkube/kni-installer/pkg/types/baremetal"
-
 	libvirttfvars "github.com/openshift-metalkube/kni-installer/pkg/tfvars/libvirt"
 	"github.com/pkg/errors"
+	"github.com/rodaine/hclencoder"
 )
 
 type config struct {
-	LibvirtURI      string `json:"libvirt_uri,omitempty"`
-	IronicURI       string `json:"ironic_uri,omitempty"`
-	Image           string `json:"os_image,omitempty"`
-	BareMetalBridge string `json:"baremetal_bridge,omitempty"`
-	OverCloudBridge string `json:"overcloud_bridge,omitempty"`
+	LibvirtURI      string `hcl:"libvirt_uri,omitempty"`
+	IronicURI       string `hcl:"ironic_uri,omitempty"`
+	Image           string `hcl:"os_image,omitempty"`
+	BareMetalBridge string `hcl:"baremetal_bridge,omitempty"`
+	OverCloudBridge string `hcl:"overcloud_bridge,omitempty"`
 
 	// Data required for masters deployment - several maps per master, because of terraform's
 	// limitation that maps cannot be strings
-	Master0     interface{} `json:"master_0"`
-	Properties0 interface{} `json:"properties_0"`
-	RootDevice0 interface{} `json:"root_device_0"`
-	DriverInfo0 interface{} `json:"driver_info_0"`
+	MasterNodes interface{} `hcl:"master_nodes"`
+	Properties  interface{} `hcl:"properties"`
+	RootDevices interface{} `hcl:"root_devices"`
+	DriverInfos interface{} `hcl:"driver_infos"`
 
-	Master1     interface{} `json:"master_1"`
-	Properties1 interface{} `json:"properties_1"`
-	RootDevice1 interface{} `json:"root_device_1"`
-	DriverInfo1 interface{} `json:"driver_info_1"`
-
-	Master2     interface{} `json:"master_2"`
-	Properties2 interface{} `json:"properties_2"`
-	RootDevice2 interface{} `json:"root_device_2"`
-	DriverInfo2 interface{} `json:"driver_info_2"`
-
-	MasterConfiguration baremetal.MasterConfiguration `json:"master_configuration"`
+	MasterConfiguration map[string]interface{} `hcl:"master_configuration"`
 }
 
 // TFVars generates bare metal specific Terraform variables.
-func TFVars(libvirtURI, ironicURI, osImage, baremetalBridge, overcloudBridge string, nodes map[string]interface{}, configuration baremetal.MasterConfiguration) ([]byte, error) {
+func TFVars(libvirtURI, ironicURI, osImage, baremetalBridge, overcloudBridge string, nodes map[string]interface{}, configuration map[string]interface{}) ([]byte, error) {
 	osImage, err := libvirttfvars.CachedImage(osImage)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to use cached libvirt image")
@@ -49,20 +37,12 @@ func TFVars(libvirtURI, ironicURI, osImage, baremetalBridge, overcloudBridge str
 		Image:               osImage,
 		BareMetalBridge:     baremetalBridge,
 		OverCloudBridge:     overcloudBridge,
-		Master0:             nodes["master_0"],
-		Properties0:         nodes["properties_0"],
-		RootDevice0:         nodes["root_device_0"],
-		DriverInfo0:         nodes["driver_info_0"],
-		Master1:             nodes["master_1"],
-		Properties1:         nodes["properties_1"],
-		RootDevice1:         nodes["root_device_1"],
-		DriverInfo1:         nodes["driver_info_1"],
-		Master2:             nodes["master_2"],
-		Properties2:         nodes["properties_2"],
-		RootDevice2:         nodes["root_device_2"],
-		DriverInfo2:         nodes["driver_info_2"],
+		MasterNodes:         nodes["master_nodes"],
+		Properties:          nodes["properties"],
+		RootDevices:         nodes["root_devices"],
+		DriverInfos:         nodes["driver_infos"],
 		MasterConfiguration: configuration,
 	}
 
-	return json.MarshalIndent(cfg, "", "  ")
+	return hclencoder.Encode(cfg)
 }
