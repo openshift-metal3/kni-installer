a75dde3fd5011567defb0479b793daf62dd57f95
Ben Nemec <bnemec@redhat.com>
2019-05-16 15:28:38 -0500

    Point Ignition at API VIP directly
    OpenShift has moved to using api-int.domain as the DNS name for
    internal API communication. This is problematic for baremetal because
    we don't want to require users to configure an internal DNS name in
    their external DNS, and we can't configure resolv.conf on the masters
    to point at our coredns until after Ignition has run.
    
    This change points Ignition directly at the API VIP specified by
    the user in install-config. It only applies to baremetal - other
    platforms will continue to use api-int.
    
    Ref: https://bugzilla.redhat.com/show_bug.cgi?id=1685704

--- pkg/asset/ignition/machine/node.go
+++ pkg/asset/ignition/machine/node.go
@@ -13,6 +13,12 @@ import (
 // pointerIgnitionConfig generates a config which references the remote config
 // served by the machine config server.
 func pointerIgnitionConfig(installConfig *types.InstallConfig, rootCA []byte, role string) *ignition.Config {
+	ignitionHost := fmt.Sprintf("api-int.%s:22623", installConfig.ClusterDomain())
+	if installConfig.Platform.BareMetal != nil {
+		// Baremetal needs to point directly at the VIP because we don't have a
+		// way to configure DNS before Ignition runs.
+		ignitionHost = fmt.Sprintf("%s:22623", installConfig.BareMetal.ApiVIP)
+	}
 	return &ignition.Config{
 		Ignition: ignition.Ignition{
 			Version: ignition.MaxVersion.String(),
@@ -21,7 +27,7 @@ func pointerIgnitionConfig(installConfig *types.InstallConfig, rootCA []byte, ro
 					Source: func() *url.URL {
 						return &url.URL{
 							Scheme: "https",
-							Host:   fmt.Sprintf("api-int.%s:22623", installConfig.ClusterDomain()),
+							Host:   ignitionHost,
 							Path:   fmt.Sprintf("/config/%s", role),
 						}
 					}().String(),
