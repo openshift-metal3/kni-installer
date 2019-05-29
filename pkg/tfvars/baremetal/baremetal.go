0de1c225161411ca24631442957943176d836ba9
Stephen Benjamin <stephen@redhat.com>
2019-05-29 15:52:23 -0400

    Run terraform 0.12 upgrade tool on baremetal platform

--- pkg/tfvars/baremetal/baremetal.go
+++ pkg/tfvars/baremetal/baremetal.go
@@ -2,26 +2,26 @@
 package baremetal
 
 import (
+	"encoding/json"
 	libvirttfvars "github.com/openshift-metalkube/kni-installer/pkg/tfvars/libvirt"
 	"github.com/pkg/errors"
-	"github.com/rodaine/hclencoder"
 )
 
 type config struct {
-	LibvirtURI         string `hcl:"libvirt_uri,omitempty"`
-	IronicURI          string `hcl:"ironic_uri,omitempty"`
-	Image              string `hcl:"os_image,omitempty"`
-	ExternalBridge     string `hcl:"external_bridge,omitempty"`
-	ProvisioningBridge string `hcl:"provisioning_bridge,omitempty"`
+	LibvirtURI         string `json:"libvirt_uri,omitempty"`
+	IronicURI          string `json:"ironic_uri,omitempty"`
+	Image              string `json:"os_image,omitempty"`
+	ExternalBridge     string `json:"external_bridge,omitempty"`
+	ProvisioningBridge string `json:"provisioning_bridge,omitempty"`
 
 	// Data required for masters deployment - several maps per master, because of terraform's
 	// limitation that maps cannot be strings
-	MasterNodes interface{} `hcl:"master_nodes"`
-	Properties  interface{} `hcl:"properties"`
-	RootDevices interface{} `hcl:"root_devices"`
-	DriverInfos interface{} `hcl:"driver_infos"`
+	MasterNodes interface{} `json:"master_nodes"`
+	Properties  interface{} `json:"properties"`
+	RootDevices interface{} `json:"root_devices"`
+	DriverInfos interface{} `json:"driver_infos"`
 
-	MasterConfiguration map[string]interface{} `hcl:"master_configuration"`
+	MasterConfiguration map[string]interface{} `json:"master_configuration"`
 }
 
 // TFVars generates bare metal specific Terraform variables.
@@ -44,5 +44,5 @@ func TFVars(libvirtURI, ironicURI, osImage, externalBridge, provisioningBridge s
 		MasterConfiguration: configuration,
 	}
 
-	return hclencoder.Encode(cfg)
+	return json.MarshalIndent(cfg, "", "  ")
 }
