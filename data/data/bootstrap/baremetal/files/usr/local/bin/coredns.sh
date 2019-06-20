9eb8f596f08e6b4e563dcdc093eaf70bac2b9034
Ben Nemec <bnemec@redhat.com>
2019-06-20 09:27:28 -0500

    Fix shellcheck issues
    Shellcheck messages fixed:
    /usr/local/bin/coredns.sh:9:1: note: read without -r will mangle
    backslashes. [SC2162]
    ./data/data/bootstrap/baremetal/files/usr/local/bin/coredns.sh:9:32:
    note: Double quote to prevent globbing and word splitting. [SC2086]
    ./data/data/bootstrap/baremetal/files/usr/local/bin/coredns.sh:13:1:
    warning: SUBNET_CIDR appears unused. Verify use (or export if used externally). [SC2034]
    ./data/data/bootstrap/baremetal/files/usr/local/bin/keepalived.sh:17:18:
    note: Double quote to prevent globbing and word splitting. [SC2086]
    
    Fixes #118

--- data/data/bootstrap/baremetal/files/usr/local/bin/coredns.sh
+++ data/data/bootstrap/baremetal/files/usr/local/bin/coredns.sh
@@ -6,11 +6,9 @@ mkdir --parents /etc/keepalived
 
 API_DNS="$(sudo awk -F[/:] '/apiServerURL/ {print $5}' /opt/openshift/manifests/cluster-infrastructure-02-config.yml)"
 CLUSTER_DOMAIN="${API_DNS#*.}"
-read -d '.' -a CLUSTER_ARR <<< $CLUSTER_DOMAIN
+read -r -d '.' -a CLUSTER_ARR <<< "$CLUSTER_DOMAIN"
 CLUSTER_NAME=${CLUSTER_ARR[0]}
 API_VIP="$(dig +noall +answer "$API_DNS" | awk '{print $NF}')"
-IFACE_CIDRS="$(ip addr show | grep -v "scope host" | grep -Po 'inet \K[\d.]+/[\d.]+' | xargs)"
-SUBNET_CIDR="$(/usr/local/bin/get_vip_subnet_cidr "$API_VIP" "$IFACE_CIDRS")"
 DNS_VIP="$(dig +noall +answer "ns1.${CLUSTER_DOMAIN}" | awk '{print $NF}')"
 grep -Ev "${DNS_VIP}|127.0.0.1" /etc/resolv.conf | tee /etc/coredns/resolv.conf
 NUM_DNS_MEMBERS=$(grep -A 5 'controlPlane' /opt/openshift/manifests/cluster-config.yaml | awk '/replicas/ {print $2}')
